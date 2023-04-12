b49c6215bd76bbe6c35327601540e23413b172f8
firdaws1234 <nazarovfirdavs12341234@gmail.com>
2023-04-12 14:54:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// task1
+// function circuitPower(a,b){
+//     return a*b
+// }
+// console.log(circuitPower(230,10));
+// task2
+// function divisibleByFive(a){
+//     if(a%5==0)
+//     return true
+//     else
+//     return false
+// }
+// console.log(divisibleByFive(55));
+// task3
+// function isSameNum(a,b){
+//     if(a==b)
+//     return true
+//     else
+//     return false
+// }
+// console.log(isSameNum(2,3));
+// task4
+// function longestTime(a,b,c){
+//     let d=a*3600
+//     let f=b*60
+//     if(d>b&&d>c)
+//     return a
+//     if(f>d&&f>c)
+//     return b
+//     if(c>d&&c>b)
+//     return c
+// }
+// console.log(longestTime(2,300,15000));
+// task5
+// function oneOddOneEven(a){
+//     let b=a%10
+//     let c=a/10
+//     if(b%2==0 && c%2!=0)
+//     return true
+//     else
+//     return false
+// }
+// console.log(oneOddOneEven(14));
+// task6
+// function negativeNum(a){
+//     let c=a*(-1)
+//     return c
+// }
+// console.log(negativeNum(8));
